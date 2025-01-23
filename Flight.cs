990df8291fab580559ac211342525f9b0751cfb8
s10268302 <s10268302@connect.np.edu.sg>
2025-01-23 19:31:01 +0800

    Flight class

---
+++ Flight.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Flight
+{
+	public string FlightName { get; set; }
+	public string Origin { get; set; }
+	public string Destination { get; set; }
+	public DateTime ExpectedTime { get; set; }
+	public string Status { get; set; }
+
+	public abstract double CalculateFees();
+
+
+	public Flight (string Flightname , string origin  , string dest, DateTime Expectedtime, string status )
+	{
+		this.FlightName = Flightname;
+		this.Origin = origin;
+		this.Destination = dest;
+		this.ExpectedTime = Expectedtime;
+		this.Status = status;
+	}
+	public Flight() { }	
+}
